22c37f4d4fe39d6a06310ad9244bd2e169210e33
lancecrowder <thelancecrowder@gmail.com>
2018-06-05 09:54:39 -0500

    Ensure that users can only visit the /profile page if they are logged in - completed

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
@@ -11,15 +12,19 @@ public class LoginServlet extends HttpServlet {
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
-            response.sendRedirect("/profile");
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "true");
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
         } else {
-            response.sendRedirect("/login");
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "false");
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
         }
     }
 }
